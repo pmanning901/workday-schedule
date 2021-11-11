c49fa83bf99d350e15eef57b21661dcf98af8e18
pmnni87@gmail.com <91038339+pmanning901@users.noreply.github.com>
2021-11-11 13:40:03 -0600

    finishing local storage and file updates

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,30 +1,64 @@
-$("#timeHere").html(moment().format('dddd, MMMM Do'))
 
-$("tr").each(function() {
-    var block = localStorage.getItem(this.id)
-    $(this).children('.second').html(block);
-    colorCode(this.id);
-  });
+// VARIABLES
 
-$(".third").click(function () {
- var block = $(this).parent('tr').prop('id')
- var textInput = $(this).parent('tr').children('.second').html();
- localStorage.setItem(block, textInput);
+var svBtn = $(".svBtn");
+
+//  FUNCTIONS
+
+// current day is displayed at the top of the calendar
+$("#curDay").text(moment().format('dddd MMMM Do YYYY'));
+
+//  time blocks are color-coded to indicate whether it is in the past, present, or future
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-box").each(function () {
+        var currHour = parseInt($(this).attr("id"));
+
+        // console.log(this); //each time-box
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+// WHEN I click the save button for that time block
+svBtn.on("click", function () {
+
+    // console.log(this); //save button
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, plan);
 });
 
-function colorCode(blockId) {
-    var block = $("#" + blockId).children('.second')
-    if (((parseInt(moment().format('H'))) > (parseInt(blockId.slice(1))))) {
-        // past #c0b9b9
-        block.attr("style", "background-color: #c0b9b9;")
-    }
-    else if (((parseInt(moment().format('H'))) < (parseInt(blockId.slice(1))))) {
-        // future #9ae591
-        block.attr("style", "background-color: #9ae591;")
-    }
-    else {
-        // current #f36060
-        block.attr("style", "background-color: #f36060;")
-    }
+// WHEN I refresh the page
+// THEN the saved events persist
+function usePlanner() {
+
+    $(".hour").each(function () {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        // console.log(this);
+        // console.log(currHour);
+
+        if (currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
 }
+
+/**
+ * CALL FUNCTIONS
+ */
+
+timeBlockColor();
+usePlanner();
 
